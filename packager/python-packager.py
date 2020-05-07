fdfe2ddefef28ecd9d13acd3f2b88f1e3a83590b
xw <xw@localhost.localdomain>
2020-05-08 00:16:09 +0800

    add license info

--- packager/python-packager.py
+++ packager/python-packager.py
@@ -1,3 +1,19 @@
+#******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2018-2019. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# Author: Shinwell_Hu Myeuler
+# Create: 2020-05-07
+# Description: provide a tool to package python module automatically
+# ******************************************************************************/
+
+
 #!/usr/bin/python3
 
 from urllib import request
