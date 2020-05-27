bebe74f810f12e6e27d2fddcf16ff9d72586b818
Shinwell Hu <huxinwei@huawei.com>
2020-05-27 10:26:08 +0000

    get list of tc members from gitee

--- tc_reminder.py
+++ tc_reminder.py
@@ -10,6 +10,7 @@ import argparse
 import json
 import sys
 import os
+import yaml
 from pprint import pprint
 from datetime import datetime
 
@@ -18,16 +19,16 @@ class Advisor:
         self.secret = open(os.path.expanduser("~/.gitee_token.json"), "r")
         self.token = json.load(self.secret)
 #        self.req = urllib.request.Request('https://gitee.com/api/v5/repos/#{owner}/issues')
-        self.header = {"Content-Type": "application/json;charset=UTF-8", "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"}
-        self.list_url = "https://gitee.com/api/v5/repos/openeuler/community/pulls?access_token={token}&state=open&sort=created&direction=desc&page=1&per_page=100"
-        self.desc_url = "https://gitee.com/api/v5/repos/openeuler/community/pulls/{number}/comments?access_token={token}&page=1&per_page=100"
-        self.tc_members = ["myeuler", "cynthia_xh", "shinwell_hu", "dream0819", "hanjun-guo", "xiexiuqi", "zhanghailiang_lucky"]
+        self.header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+        self.tc_members = None
         self.time_format = "%Y-%m-%dT%H:%M:%S%z"
 
 
-    def get_gitee(self, url):
+    def get_json(self, url):
+        headers = self.header.copy()
+        headers["Content-Type"] = "application/json;charset=UTF-8"
         req = urllib.request.Request(url = url, 
-                headers = self.header,
+                headers = headers,
                 method = "GET")
        
         with urllib.request.urlopen(req) as u:
@@ -35,16 +36,37 @@ class Advisor:
         return resp
 
 
+    def get_file(self, repo, path):
+        url = "https://gitee.com/{repo}/raw/master/{path}".format(repo=repo, path=path)
+        req = urllib.request.Request(url = url, 
+                headers = self.header,
+                method = "GET")
+       
+        with urllib.request.urlopen(req) as u:
+            resp = u.read()
+
+        return resp
+
     def get_prs(self):
-        url = self.list_url.format(token=self.token["access_token"])
-        return self.get_gitee(url)
+        list_url = "https://gitee.com/api/v5/repos/openeuler/community/pulls?access_token={token}&state=open&sort=created&direction=desc&page=1&per_page=100"
+        url = list_url.format(token=self.token["access_token"])
+        return self.get_json(url)
 
     def get_pr_comments(self, number):
-        url = self.desc_url.format(number=number, token=self.token["access_token"])
-        return self.get_gitee(url)
+        desc_url = "https://gitee.com/api/v5/repos/openeuler/community/pulls/{number}/comments?access_token={token}&page=1&per_page=100"
+        url = desc_url.format(number=number, token=self.token["access_token"])
+        return self.get_json(url)
+
+    def get_tc_members(self):
+        m = yaml.load(adv.get_file("openeuler/community", "sig/TC/OWNERS"), Loader=yaml.Loader)
+        self.tc_members = m["maintainers"]
+        return m["maintainers"]
 
     def filter_out_tc(self, users):
+        if not self.tc_members:
+            self.get_tc_members()
         return [x for x in self.tc_members if x in users]
+        
         
 if __name__ == "__main__":
     par = argparse.ArgumentParser()
@@ -63,9 +85,6 @@ if __name__ == "__main__":
         for c in comm:
             #print("comment updated at:")
             #pprint(datetime.strptime(c["updated_at"], adv.time_format)) 
-            #print("PR updated at:")
-            #pprint(datetime.strptime(last_update, adv.time_format))
-            #if datetime.strptime(c["updated_at"], adv.time_format) < datetime.strptime(last_update, adv.time_format):
             users.append(c["user"]["login"]) 
         tc = adv.filter_out_tc(users)
         print("Currently involved TC members: " + ", ".join(tc) + "\n")
