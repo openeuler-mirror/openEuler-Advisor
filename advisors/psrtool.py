ef1fc17ee6a536a22bf67fd4b60849101a50ba5e
Shinwell Hu <micromotive@qq.com>
2020-08-22 05:03:35 +0000

    clean up pylint hints

--- advisors/psrtool.py
+++ advisors/psrtool.py
@@ -10,60 +10,85 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 #
-# This is a helper script provides a way to query or list packages in specific SIG.
-# This script was inspired by previous work from @love_hangzhou
 # ******************************************************************************/
+"""
+This is a helper script provides a way to query or list packages in specific SIG.
+This script was inspired by previous work from @love_hangzhou
+"""
 
-import yaml, argparse, re, os
+import re
+import argparse
+import yaml
 
-def list_packages(s, sig_name):
-    for sig in s:
+def list_packages(sigs, sig_name):
+    """
+    List all packages managed by specific SIG
+    """
+    for sig in sigs:
         if sig['name'].lower() == sig_name.lower():
             return sig['repositories']
+    return []
 
 
-def list_sigs(s):
+def list_sigs(sigs):
+    """
+    List all current SIGs
+    """
     result = []
-    for sig in s:
+    for sig in sigs:
         result.append(sig['name'])
     return result
 
 
-def package_to_sigs(s, pkg_names):
+def package_to_sigs(sigs, pkg_names):
+    """
+    Query which SIG manages the packages.
+    """
     result = {}
     for pkg in pkg_names:
-        for sig in s:
+        for sig in sigs:
             repos = sig['repositories']
             for repo in repos:
-                searchObj = re.search(pkg.lower(), repo.lower(), 0)
-                if searchObj:
+                search_obj = re.search(pkg.lower(), repo.lower(), 0)
+                if search_obj:
                     result[repo] = sig['name']
     return result
 
 
-def print_list(l):
-    for i in l:
+def print_list(lista):
+    """
+    Helper for print list
+    """
+    for i in lista:
         print(i)
 
-def print_dict(d):
-    for k in d.keys():
-        print(k + ": " + d[k])
 
-if __name__ == "__main__":
+def print_dict(dicta):
+    """
+    Helper for print dictionary
+    """
+    for k in dicta.keys():
+        print(k + ": " + dicta[k])
+
+
+def main():
+    """
+    Main entrance of functionality
+    """
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-l", "--list", default=False, nargs='?', 
-            help="List packages managed by the specific SIG, or list all SIGs instead")
-    group.add_argument("-q", "--query_packages", default=False, nargs='+', 
-            help="Query which SIG manage the specific package")
-    parser.add_argument("-f", "--yaml", default="sig/sigs.yaml", 
-            help="Local path of sigs.yaml file")
+    group.add_argument("-l", "--list", default=False, nargs='?',
+                       help="List packages managed by the specific SIG, or list all SIGs instead")
+    group.add_argument("-q", "--query_packages", default=False, nargs='+',
+                       help="Query which SIG manage the specific package")
+    parser.add_argument("-f", "--yaml", default="sig/sigs.yaml",
+                        help="Local path of sigs.yaml file")
 
     args = parser.parse_args()
 
     try:
-        with open(args.yaml, 'r') as f:
-            sigs = yaml.load(f, Loader=yaml.Loader)['sigs']
+        with open(args.yaml, 'r') as yaml_file:
+            sigs = yaml.load(yaml_file, Loader=yaml.Loader)['sigs']
     except IOError:
         print("Failed to load information from %s" % args.yaml)
         parser.print_help()
@@ -79,3 +104,5 @@ if __name__ == "__main__":
         pass
     exit(0)
 
+if __name__ == "__main__":
+    main()
