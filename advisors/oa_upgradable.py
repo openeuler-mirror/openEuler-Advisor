b20539fa12bb4b346662d412b08c9aaf252346a0
Leo Fang <leofang_94@163.com>
2020-10-10 14:44:54 +0800

    Clean code and bugfix 1.pylint fix for gitee.py 2.add copyright for oa_upgradable and simple_update_robot 3.bugfix for specfile_exceptions
    Signed-off-by: Leo Fang <leofang_94@163.com>

--- advisors/oa_upgradable.py
+++ advisors/oa_upgradable.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+#******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+#
+# ******************************************************************************/
 """
 This is a script to check upgradable information against upstream
 """
@@ -12,7 +24,6 @@ import yaml
 import gitee
 import check_upstream
 import version_recommend
-
 
 
 def _filter_except(excpts, sources):
