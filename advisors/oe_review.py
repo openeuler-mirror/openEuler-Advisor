c20511b738f262621de1dc7ca626a9f6e46db899
Shinwell Hu <huxinwei@huawei.com>
2024-10-26 16:27:06 +0800

    init threading support in heavy review batch

--- advisors/oe_review.py
+++ advisors/oe_review.py
@@ -52,6 +52,46 @@ patches from easy patches, core changes from leaf changes. Please evaluate the b
 a rating number in range 1-100.
 """
 
+# define data structure that contains queue and mutex lock for thread sharing
+import threading
+import time
+
+class ThreadSafeQueue:
+    def __init__(self):
+        self.queue = []  # Your data structure (list) can be replaced with any other like deque from collections
+        self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
+
+    def put(self, item):
+        with self.lock:
+            self.queue.append(item)
+            self.condition.notify_all()  # Notify all waiting threads that new item is added
+
+    def get(self):
+        with self.lock:
+            while len(self.queue) == 0:
+                self.condition.wait()  # Wait until there are items in the queue
+            item = self.queue.pop(0)
+            return item
+
+    def qsize(self):
+        with self.lock:
+            return len(self.queue)
+
+
+# 建三个队列，一个是待处理PR列表，一个是经过预处理的PR列表，一个是待提交PR列表
+# 批处理，首先关闭所有可以关闭的PR，直接合并sync且没有ci_failed的PR
+# 然后对所有其他的PR再进行review
+# define 3 queues to be shared across threads
+# List of PRs to be reviewed, by review_repos()
+PENDING_PRS = ThreadSafeQueue()
+# review_pr() get pr from PENDING_PRS, if can be obviously handled, put comment into submitting_prs, otherwise, move to NEED_REVIEW_PRS
+# that are being preprocessed for review
+NEED_REVIEW_PRS = ThreadSafeQueue()
+MANUAL_REVIEW_PRS = ThreadSafeQueue()
+# PRs that are being submitted 
+SUBMITTING_PRS = ThreadSafeQueue()
+
 def generate_review_from_ollama(pr_content, prompt, model="llama3.1:8b"):
     base_url = "http://localhost:11434/api"
     json_resp = []
@@ -127,7 +167,149 @@ def edit_content(text, editor):
         subprocess.call([editor, path])
         text_new = open(path).read()
         return text_new
+
+def easy_classify(pull_request):
+    suggest_action = ""
+    suggest_reason = ""
+    sync_pr = False
     
+    if not pull_request["mergeable"]:
+        suggest_action = "/close"
+        suggest_reason = "存在冲突"
+
+    if pull_request["title"].startswith("[sync] PR-") and pull_request["user"]["login"]=="openeuler-sync-bot":
+        sync_pr = True
+
+    for label in pull_request["labels"]:
+        if label['name'] == "ci_failed":
+            suggest_action = "/close"
+            suggest_reason = "CI 失败"
+        elif label['name'] == 'openeuler-cla/no':
+            suggest_action = "/check-cla"
+            suggest_reason = "CLA 未签署"
+        elif label['name'] == 'ci_processing':
+            suggest_action = "暂不处理"
+            suggest_reason = "等待 CI 处理结果"
+        elif label['name'] == 'kind/wait_for_update':
+            suggest_action = "暂不处理"
+            suggest_reason = "等待提交人更新"
+        elif label['name'] == 'wait_confirm':
+            suggest_action = "暂不处理"
+            suggest_reason = "等待相关开发者确认"
+        elif label['name'] == 'ci_successful':
+            if sync_pr == True:
+                suggest_action = "/lgtm\n/approve"
+                suggest_reason = "分支同步 PR，构建成功，默认合入。"
+        else:
+            pass
+    return suggest_action, suggest_reason
+
+def sort_pr(user_gitee):
+    while True:
+        item = PENDING_PRS.get()
+        if not item:
+            break
+        print(f"Got {item} from queue")
+
+        pull_request = user_gitee.get_pr(item["repo"], item["number"], item["owner"])
+
+        suggest_action, suggest_reason = easy_classify(pull_request)
+
+        if suggest_action == "":
+            need_review_pr = {}
+            need_review_pr['pull_request'] = pull_request
+            need_review_pr['pr_info'] = item
+            NEED_REVIEW_PRS.put(need_review_pr)
+        else:
+            review_comment_raw = suggest_action + "\n" + suggest_reason
+            submitting_pr = {}
+            submitting_pr['review_comment'] = review_comment_raw
+            submitting_pr['pr_info'] = item
+            submitting_pr['suggest_action'] = suggest_action
+            submitting_pr['suggest_reason'] = suggest_reason
+            SUBMITTING_PRS.put(submitting_pr)
+
+    NEED_REVIEW_PRS.put(None)
+
+def ai_review(user_gitee):
+    while True:
+        item = NEED_REVIEW_PRS.get()
+        if not item:
+            break
+        pr_info = item["pr_info"]
+        pr_diff = user_gitee.get_diff(pr_info['repo'], pr_info['number'], pr_info['owner'])
+        if not pr_diff:
+            print("Failed to get PR:%s of repository:%s/%s, make sure the PR is exist." % (pr_info['number'], pr_info['owner'], pr_info['repo']))
+            continue
+        review = generate_review_from_ollama(pr_diff, OE_REVIEW_PR_PROMPT)
+        review_rating = generate_review_from_ollama(pr_diff, OE_REVIEW_RATING_PROMPT)
+    
+        manual_review_pr = {}
+        manual_review_pr['pr_info'] = pr_info
+        manual_review_pr['pull_request'] = item['pull_request']
+        manual_review_pr['pr_diff'] = pr_diff
+        manual_review_pr['review'] = review
+        manual_review_pr['review_rating'] = review_rating
+        MANUAL_REVIEW_PRS.put(manual_review_pr)
+    MANUAL_REVIEW_PRS.put(None)
+
+def manually_review(user_gitee, editor):
+    while True:
+        item  = MANUAL_REVIEW_PRS.get()
+        if not item:
+            break
+        review_content = ""
+        pull_request = item['pull_request']
+        pr_info = item["pr_info"]
+        review = item['review']
+        review_rating = item['review_rating']
+        pr_diff = item['pr_diff']
+
+        review_content += "!{number}: {title}\n# {body}\n".format(number=pull_request["number"], title=pull_request["title"], body=pull_request["body"])
+
+        review_content += "This PR has following labels:\n"
+        for label in pull_request["labels"]:
+            review_content += f"{label['name']}  "
+            
+        review_comment_raw = edit_content(review_content + '\n\n' + review + '\n\n' + review_rating + '\n\n' + pr_diff, editor)
+
+        submitting_pr = {}
+        submitting_pr['review_comment'] = review_comment_raw
+        submitting_pr['pr_info'] = item['pr_info']
+        submitting_pr['pull_request'] = pull_request
+        SUBMITTING_PRS.put(submitting_pr)
+
+    SUBMITTING_PRS.put(None)
+
+def submit_review_impl(user_gitee, pr_info, pull_request, review_comment, suggest_action="", suggest_reason=""):
+    result = " is handled and review is published."
+
+    user_gitee.create_pr_comment(pr_info['repo'], pr_info['number'], review_comment, pr_info['owner'])
+
+    if suggest_action == "/close":
+        result = " is closed due to {reason}.".format(reason=suggest_reason)
+    elif suggest_action == "暂不处理":
+        result = " is skipped due to {reason}.".format(reason=suggest_reason)
+    elif suggest_action == "/lgtm\n/approve":
+        result = " is approved due to {reason}.".format(reason=suggest_reason)
+    else:
+        pass
+    print("!{number}: {title}{res}".format(number=pr_info["number"], title=pull_request["title"], res=result))
+
+def submmit_review(user_gitee):
+    while True:
+        item = SUBMITTING_PRS.get()
+        print(item)
+        if not item:
+            break
+        review_comment = item['review_comment']
+        pr_info = item['pr_info']
+        pull_request = item['pull_request']
+        suggest_action = item.get('suggest_action', "")
+        suggest_reason = item.get('suggest_reason', "")
+
+        submit_review_impl(user_gitee, pr_info, pull_request, review_comment, suggest_action, suggest_reason)
+
 
 def review_pr(user_gitee, repo_name, pull_id, group, editor):
     """
@@ -140,19 +322,22 @@ def review_pr(user_gitee, repo_name, pull_id, group, editor):
 
     result = ""
 
-    review_content += "# !{number}: {title}\n# {body}\n".format(number=pull_request["number"], title=pull_request["title"], body=pull_request["body"])
+    review_content += "!{number}: {title}\n# {body}\n".format(number=pull_request["number"], title=pull_request["title"], body=pull_request["body"])
     if not pull_request["mergeable"]:
-        review_content += "# This PR is not mergeable!\n"
+        review_content += "This PR is not mergeable!\n"
         suggest_action = "/close"
         suggest_reason = "存在冲突"
 
-    review_content += "# This PR has following labels:\n"
+    if pull_request["title"].startswith("[sync] PR-") and pull_request["user"]["login"]=="openeuler-sync-bot":
+        sync_pr = True
+
+    review_content += "This PR has following labels:\n"
     for label in pull_request["labels"]:
         if label['name'] == "ci_failed":
             suggest_action = "/close"
             suggest_reason = "CI 失败"
         elif label['name'] == 'openeuler-cla/no':
-            suggest_action = "/close"
+            suggest_action = "/check-cla"
             suggest_reason = "CLA 未签署"
         elif label['name'] == 'ci_processing':
             suggest_action = "暂不处理"
@@ -163,12 +348,15 @@ def review_pr(user_gitee, repo_name, pull_id, group, editor):
         elif label['name'] == 'wait_confirm':
             suggest_action = "暂不处理"
             suggest_reason = "等待相关开发者确认"
+        elif label['name'] == 'ci_successful':
+            if sync_pr == True:
+                suggest_action = "/lgtm\n/approve"
+                suggest_reason = "分支同步 PR，构建成功，默认合入。"
         else:
             pass
         review_content += f"{label['name']}  "
     review_content += "\n\n"
 
-        
     pr_diff = user_gitee.get_diff(repo_name, pull_id, group)
     if not pr_diff:
         print("Failed to get PR:%s of repository:%s/%s, make sure the PR is exist." % (pull_id, group, repo_name))
@@ -177,8 +365,14 @@ def review_pr(user_gitee, repo_name, pull_id, group, editor):
     if suggest_action == "/close":
         review = "/close"
         review_comment_raw = review + "\n" + suggest_reason
+    elif suggest_action == "/check-cla":
+        review = "/check-cla"
+        review_comment_raw = review + "\n" + suggest_reason
     elif suggest_action == "暂不处理":
         review = "暂不处理"
+        review_comment_raw = review + "\n" + suggest_reason
+    elif suggest_action == "/lgtm\n/approve":
+        review = "/lgtm\n/approve"
         review_comment_raw = review + "\n" + suggest_reason
     else:
         review = generate_review_from_ollama(pr_diff, OE_REVIEW_PR_PROMPT)
@@ -198,13 +392,15 @@ def review_pr(user_gitee, repo_name, pull_id, group, editor):
         result = " is closed due to {reason}.".format(reason=suggest_reason)
     elif suggest_action == "暂不处理":
         result = " is skipped due to {reason}.".format(reason=suggest_reason)
+    elif suggest_action == "/lgtm\n/approve":
+        result = " is approved due to {reason}.".format(reason=suggest_reason)
     else:
         result = " is handled and review is published."
     return result
 
-def review_repo(user_gitee, owner, repo, editor):
+def review_repo(user_gitee, owner, repo):
     """"
-    Review PRs in give repo, or doing nothing if no PR
+    Get PRs in give repo, or doing nothing if no PR
     """
     result = f'{owner}/{repo}'.format(owner=owner, repo=repo)
     PRs = user_gitee.list_pr(repo, owner)
@@ -212,9 +408,29 @@ def review_repo(user_gitee, owner, repo, editor):
         return
     else:
         for pr in PRs:
-            h_res = review_pr(user_gitee, repo, pr['number'], owner, editor)
-            result += '\n' + "!{number}: {title}{res}".format(number=pr["number"], title=pr["title"], res=h_res)
-        print(result)
+            pending_pr = {}
+            pending_pr['repo'] = repo
+            pending_pr['number'] = pr["number"]
+            pending_pr['owner'] = owner
+            #print(pending_pr)
+            PENDING_PRS.put(pending_pr)
+
+def generate_pending_prs(user_gitee, sig):
+    """
+    Generate pending PRs
+    """
+    src_oe_repos = user_gitee.get_repos_by_sig(sig)
+    for repo in src_oe_repos:
+        review_repo(user_gitee, 'src-openeuler', repo)
+
+    oe_repos = user_gitee.get_openeuler_repos_by_sig(sig)
+    for repo in oe_repos:
+        review_repo(user_gitee, 'openeuler', repo)
+
+    PENDING_PRS.put(None)
+    print("DONE PENDING GENERATE")
+    return 0
+
 
 def main():
     """
@@ -231,13 +447,24 @@ def main():
         sys.exit(1)
 
     if args.active_user:
-        src_oe_repos = user_gitee.get_repos_by_sig(args.sig)
-        for repo in src_oe_repos:
-            review_repo(user_gitee, 'src-openeuler', repo, args.editor)
+        generate_pending_prs_thread = threading.Thread(target=generate_pending_prs, args=(user_gitee, args.sig))
+        sort_pr_thread = threading.Thread(target=sort_pr, args=(user_gitee,))
+        ai_review_thread = threading.Thread(target=ai_review, args=(user_gitee,))
+        manually_review_thread = threading.Thread(target=manually_review, args=(user_gitee, args.editor))
+        submmit_review_thread = threading.Thread(target=submmit_review, args=(user_gitee,))
 
-        oe_repos = user_gitee.get_openeuler_repos_by_sig(args.sig)
-        for repo in oe_repos:
-            review_repo(user_gitee, 'openeuler', repo, args.editor)
+        generate_pending_prs_thread.start()
+        sort_pr_thread.start()
+        ai_review_thread.start()
+        manually_review_thread.start()
+        submmit_review_thread.start()
+
+        generate_pending_prs_thread.join()
+        sort_pr_thread.join()
+        ai_review_thread.join()
+        manually_review_thread.join()
+        submmit_review_thread.join()
+
         # 获取当前用户所有担任 maintainer 的 sig，以及所有担任 committer 的 代码仓
         # 此处应考虑建立本地缓存，保留PR的标签，最近更新，如果和本地缓存相比没有更新就忽略，否则更新 review
         # 然后根据不同的模型生成 review
